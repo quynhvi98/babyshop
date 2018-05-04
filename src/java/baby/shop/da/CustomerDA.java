3a0667c823f8b998588d3771f81073c446837895
viquy <viquy@10.22.186.177>
2018-05-04 16:39:20 +0700

    -config connect to Db: context.xml and web.xml

--- src/java/baby/shop/da/CustomerDA.java
+++ src/java/baby/shop/da/CustomerDA.java
@@ -16,18 +16,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.naming.NamingException;
 
 /**
  *
  * @author viquy
  */
 public class CustomerDA {
-    public List<Customer> ListCustomer() throws ClassNotFoundException {
+    public List<Customer> ListCustomer() throws ClassNotFoundException, NamingException {
         List<Customer> list = new LinkedList<>();
         try {
 
             String sql = "SELECT * FROM dbo.customer";
-            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            PreparedStatement ps = DBConnection.makeConnection().prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 list.add(new Customer(rs.getInt("id"), rs.getString("name"), rs.getString("phone"), rs.getString("address"), rs.getFloat("total")));
@@ -39,11 +40,11 @@ public class CustomerDA {
 
     }
 
-    public boolean checkOut(String name,String phone,String address, float total) throws ClassNotFoundException {
+    public boolean checkOut(String name,String phone,String address, float total) throws ClassNotFoundException, NamingException {
         int result = 0;
         try {
             String sql = "INSERT dbo.customer( name, phone, address, total )VALUES  (?,?,?, ?)";
-            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            PreparedStatement ps = DBConnection.makeConnection().prepareStatement(sql);
             ps.setString(1, name);
             ps.setString(2, phone);
             ps.setString(3, address);
